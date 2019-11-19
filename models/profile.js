a4a93b114c7113d3abcc51aebc089a7ef2587754
Chris Flick <chrisflick@gmail.com>
2019-11-19 14:27:28 -0800

    updated Game and Profile to hold a stringified version of games id and moves respectively

--- models/profile.js
+++ models/profile.js
@@ -1,36 +1,38 @@
-module.exports = function(sequelize, DataTypes) { // Creating a profile module
+module.exports = function (sequelize, DataTypes) { // Creating a profile module
     var Profile = sequelize.define("Profile", {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        primaryKey: true
-      },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            primaryKey: true
+        },
 
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-  
-      elo: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      opponentElo: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
 
-      wins: {
-          type: DataTypes.INTEGER,
-          allowNull: false
-      },
+        elo: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        opponentElo: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
 
-      loses: {
-          type: DataTypes.INTEGER,
-          allowNull: false
-      }
+        wins: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+
+        loses: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        games: {
+            type: DataTypes.STRING // Stringify of an array
+        },
     });
     return Profile;
-  };
-  
+};
