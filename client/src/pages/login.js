8b86e7d2038c7c390b485fd4f4f36cc7fc2bc4ec
Toki <actocchi@gmail.com>
2019-11-22 10:26:14 -0800

    commented out avatars and cleaned up spelling mistakes

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
+// import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -9,7 +9,6 @@ import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -64,11 +63,11 @@ export default function SignInSide() {
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
+              {/* add back if we add avatars */}
+          {/* <Avatar className={classes.avatar}>
+          </Avatar> */}
           <Typography component="h1" variant="h5">
-           Login in
+            Login
           </Typography>
           <form className={classes.form} noValidate>
             <TextField
