0584333bf93271fc4c9a7377a98cee1ebbde96f7
Ramon Sanchez <songwright66@gmail.com>
2019-11-22 14:10:46 -0800

    Updated Signup component

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import { Container } from "../components/Grid";
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import { deepOrange, deepPurple } from '@material-ui/core/colors';
+import Grid from '@material-ui/core/Grid';
 import "../css/Signup.css";
 
 const Signup = () => {
@@ -41,39 +45,78 @@ const Signup = () => {
     }
   };
 
+  const useStyles = makeStyles({
+    avatar: {
+      margin: 10,
+    },
+    orangeAvatar: {
+      margin: 10,
+      color: '#fff',
+      backgroundColor: deepOrange[500],
+    },
+    purpleAvatar: {
+      margin: 10,
+      color: '#fff',
+      backgroundColor: deepPurple[500],
+    },
+  });
+
+  const classes = useStyles();
+
   return (
     <Container fluid>
       <div>
         <header><h1>Reversi</h1></header>
-          <div className="form-container">
-            <div className="form-card">
-              <form className="form">
-              Enter User Name:
-              <input
-                value={inputState.userName}
-                name="userName"
-                type="text"
-                placeholder="User Name"
-                onChange={handleChange}
-              />
-              Enter Password
-              <input
-                value={inputState.password.substring(0, 14)}
-                name="password"
-                type="password"
-                placeholder="password"
-                onChange={handleChange}
-              />
-              Confirm Password
-              <input
-                value={inputState.passwordConfirm}
-                name="passwordConfirm"
-                type="password"
-                placeholder="password"
-                onChange={handleChange}
-              />
-              <button onClick={handleFormSubmit}>Submit</button>
+        <div className="form-container">
+          <div className="form-card">
+            <form className="form">
+              <span>
+                Enter User Name:
+                  <input
+                  value={inputState.userName}
+                  name="userName"
+                  type="text"
+                  placeholder="User Name"
+                  onChange={handleChange}
+                />
+              </span>
+              <span>
+                Enter Password:
+                <input
+                  value={inputState.password.substring(0, 14)}
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  onChange={handleChange}
+                />
+              </span>
+              <span>
+                Confirm Password
+                <input
+                  value={inputState.passwordConfirm}
+                  name="passwordConfirm"
+                  type="password"
+                  placeholder="password"
+                  onChange={handleChange}
+                />
+              </span>
             </form>
+            <Grid container justify="center" alignItems="center">
+              <Avatar className={classes.avatar}>H</Avatar>
+              <Avatar className={classes.orangeAvatar}>N</Avatar>
+              <Avatar className={classes.avatar}>G</Avatar>
+              <Avatar className={classes.purpleAvatar}>OP</Avatar>
+            </Grid>
+            <Grid container justify="center" alignItems="center">
+              <Avatar className={classes.avatar}>J</Avatar>
+              <Avatar className={classes.avatar}>Z</Avatar>
+              <Avatar className={classes.orangeAvatar}>L</Avatar>
+              <Avatar className={classes.purpleAvatar}>DV</Avatar>
+            </Grid>
+            <div className="avatar-choices">
+              <p>Avatar Choices</p>
+            <button type="button" class="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
+            </div>
           </div>
         </div>
       </div>
