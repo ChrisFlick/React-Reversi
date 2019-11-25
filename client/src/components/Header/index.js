3cb1be3dc270d6791d8c1377b96c7e9dc3175f07
Ramon Sanchez <songwright66@gmail.com>
2019-11-25 12:49:55 -0800

    Created Header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Logo from  "../../img/Reversi-logo-200.gif";
+
+function Header() {
+  return(
+    <header> <img src={Logo} alt="Reversi"/> </header>
+  );
+}
+
+export default Header;
