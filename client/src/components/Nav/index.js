fcdc78bd51e44a1773b997530eb51b91305044d1
Ramon Sanchez <songwright66@gmail.com>
2019-11-26 09:45:16 -0800

    Updated nav and nav styles

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { useStoreContext } from "../../utils/GlobalState";
+import "../../css/Nav.css";
 
 function Nav() {
   const [store] = useStoreContext();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">
-        Your personal CMS
-      </a>
+    //<nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav >
+      <span className="lobby">
+        <a href="/">Create lobby</a>
+      </span>
       {store.loading ? <a className="navbar-brand ml-auto">Loading...</a> : <></>}
     </nav>
   );
