f365de4e94732603048e4323175ab4fda9a6a4f4
Ramon Sanchez <songwright66@gmail.com>
2019-12-03 13:02:17 -0800

    Fixed Lobbies styles

--- client/src/pages/Lobbies.js
+++ client/src/pages/Lobbies.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Card from "../components/Card"
+import CardHeader from "../components/CardHeader"
+import CardBody from "../components/CardBody"
 import Header from "../components/Header"
 import Nav from "../components/Nav"
 import API from "../utils/API"
 import Avatar from '@material-ui/core/Avatar';
+import "../css/Lobbies.css"
 
 const username = localStorage.getItem("username")
 
@@ -37,21 +40,23 @@ const Lobbies = () => {
 
 
     return (
-        <div>
+        <div className="lobbies-container">
             <Header />
             <Nav />
 
-            <Row>
-                <Col size="md-2"></Col>
-                <Col size="md-4">
-                    <h1>Create a lobby</h1>
+            <div className="lobbies">
+                <div className="create-card">
+                    <CardHeader>Create a Lobby</CardHeader>
+                    <CardBody>
+
                     <form id="createLobby">
                         Name:
                         <input id="name" type="text"></input>
                     </form>
-                </Col>
-                <Col size="md-6">
-                    <h1>Join a loby</h1>
+                    </CardBody>
+                </div>
+                <div className="join-card">
+                    <h1>Join a lobby</h1>
                     {state.lobbies.map(item => {
                         if (item.hasRoom) {
                             return (
@@ -59,8 +64,8 @@ const Lobbies = () => {
                             )
                         }
                     })}
-                </Col>
-            </Row>
+                </div>
+            </div>
         </div>
     )
 }
