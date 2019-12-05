2e461f1383310e1b80066c6d8cdc11aa3a7c4d56
Ramon Sanchez <songwright66@gmail.com>
2019-12-05 02:08:52 -0800

    Update sqaure component to show color of player

--- client/src/components/Board/square.js
+++ client/src/components/Board/square.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './board.css'
-import Piece from '../Piece/index'
+import Piece from '../Piece'
+
 function Square(props) {
+    const [backgroundColor, setBackgroundColor] = useState();
+    const propsColor = useRef(0);
+
+useEffect(() => {
+    const currentColor = propsColor.current.innerHTML;
+    if (currentColor === "1") {
+        setBackgroundColor("#fff");
+    } else if (currentColor === "2") {
+        setBackgroundColor("#000");
+    } else if (currentColor === "3") {
+        setBackgroundColor("#90ee90");
+    }
+});
+
     return (
         <button className={"square"}
             onClick={props.onClick}
+            ref={propsColor}
+            style={{ backgroundColor: backgroundColor }}
         >
             {props.color}
         </button>
