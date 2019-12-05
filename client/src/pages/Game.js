8f95f19d1d828d589b990d1ba875ffc3ffa760e1
Ramon Sanchez <songwright66@gmail.com>
2019-12-05 15:18:09 -0800

    Fixed page widths

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,19 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
-// import API from "../utils/API"
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import BlackDot from "../img/black-dot.png";
 import WhiteDot from "../img/white-dot.png";
 import API from "../utils/API"
-
-// import { useStoreContext } from "../utils/GlobalState";
-// import {
-//   ADD_PEER,
-// } from "../utils/actions";
-// import { Container } from "../components/Grid";
-// import { makeStyles } from '@material-ui/core/styles';
-// import Avatar from '@material-ui/core/Avatar';
-// import { deepOrange, deepPurple } from '@material-ui/core/colors';
 import "../css/Game.css";
 import Game from '../components/Game/game';
 
@@ -95,32 +85,7 @@ const Games = () => {
       <Header />
       <div className="navbar">Proposed Navbar</div>
       <Nav />
-      {/* <div className="game-info">
-        <div className="game-details">
-          <h5>Game Details:</h5>
-          <ul style={{ padding: 0 }}>
-            <li style={{ listStyleType: "none" }}>Game ID</li>
-            <li style={{ listStyleType: "none" }}>Game Name</li>
-            <li style={{ listStyleType: "none" }}>Date</li>
-          </ul>
-        </div>
-        {/* <div className="game-details">
-          <h5>Games List:</h5>
-          <ol style={{ padding: 13 }}>
-            <li>Death Match</li>
-            <li>One More Time</li>
-            <li>Mando's Turn</li>
-          </ol>
-        </div> }
-      </div> */}
       <Game className="game-board">
-        {/* <div className="scores">
-            <div>WhiteHead 15</div>
-            <div><img src={BlackDot}/></div>
-            <div>Ramon 5</div>
-            <div> <img src={WhiteDot}/></div>
-            </div>
-          <img src={Board} alt="Reversi board"/> */}
       </Game>
       <div className="profiles">
         <div className="profile-details">
