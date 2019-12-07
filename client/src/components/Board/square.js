7b08856a4dec632810a30c2e36631f6b730fe50c
Ramon Sanchez <songwright66@gmail.com>
2019-12-07 00:17:04 -0800

    Updated graphics in Square.js

--- client/src/components/Board/square.js
+++ client/src/components/Board/square.js
@@ -1,34 +1,44 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './board.css'
-import Piece from '../Piece'
 
 function Square(props) {
     const [backgroundColor, setBackgroundColor] = useState();
+    const [border, setBorder] = useState();
     const propsColor = useRef(0);
 
-useEffect(() => {
-    const currentColor = propsColor.current.innerHTML;
-    if (currentColor === "1") {
-        setBackgroundColor("#fff");
-    } else if (currentColor === "2") {
-        setBackgroundColor("#000");
-    } else if (currentColor === "3") {
-        setBackgroundColor("#90ee90");
-    } else if (currentColor === "0") {
-        setBackgroundColor("rgb(0,153,64)");
-    }
-
-});
+    useEffect(() => {
+        const currentColor = propsColor.current.innerHTML;
+        if (currentColor === "1") {
+            setBackgroundColor("#fff");
+            setBorder("1px solid #000");
+        } else if (currentColor === "2") {
+            setBackgroundColor("#000");
+            setBorder("1px solid #000");
+        } else if (currentColor === "3") {
+            setBackgroundColor("rgb(0,153,64)");
+            setBorder("1px solid #90ee90");
+        } else if (currentColor === "0") {
+            setBackgroundColor("rgb(0,153,64)")
+            setBorder("0px solid rgb(0,153,64)");
+        }
+    });
 
     return (
         <button className={"square"}
             onClick={props.onClick}
-            style={{ backgroundColor: backgroundColor }}
         >
-            <div style={{display: "none"}} ref={propsColor}>
-            
+            <div style={{
+                height: '25px',
+                width: '25px',
+                backgroundColor: backgroundColor,
+                borderRadius: '50%',
+                border: border,
+                display: 'inline-block',
+            }}
+            ></div>
+            <div style={{ display: "none" }} ref={propsColor}>
                 {props.color}
-                </div>
+            </div>
         </button>
     );
 }
