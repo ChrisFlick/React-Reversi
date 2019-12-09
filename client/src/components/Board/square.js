595b54a5aba554af837ff0f4888fe19d6aff9fc0
Ramon Sanchez <songwright66@gmail.com>
2019-12-08 18:42:21 -0800

    Changed if statements to a switch

--- client/src/components/Board/square.js
+++ client/src/components/Board/square.js
@@ -9,28 +9,36 @@ function Square(props) {
 
     useEffect(() => {
         const currentColor = propsColor.current.innerHTML;
-        if (currentColor === "1") {
-            setBackground("linear-gradient(30deg, rgba(255, 255, 255, 0.5) 0%," +
-            "rgba(203, 203, 203, 0.44) 11.02%, rgba(150, 150, 150, 0.38) 23.9%," +
-            "rgba(104, 104, 104, 0.32) 36.96%, rgba(67, 67, 67, 0.25) 49.92%," +
-            "rgba(37, 37, 37, 0.19) 62.76%, rgba(17, 17, 17, 0.12) 75.46%," +
-            "rgba(4, 4, 4, 0.06) 87.95%, rgba(0, 0, 0, 0) 100%)");
-            setBackgroundColor("#fff");
-            setBorder("1px solid #000");
-        } else if (currentColor === "2") {
-            setBackground("linear-gradient(210deg, rgba(255, 255, 255, 0.7) 0%," +
-            "rgba(203, 203, 203, 0.62) 11.02%, rgba(150, 150, 150, 0.53) 23.9%," +
-            "rgba(104, 104, 104, 0.44) 36.96%, rgba(67, 67, 67, 0.35) 49.92%," +
-            "rgba(37, 37, 37, 0.26) 62.76%, rgba(17, 17, 17, 0.17) 75.46%," +
-            "rgba(4, 4, 4, 0.08) 87.95%, rgba(0, 0, 0, 0) 100%)");
-            setBackgroundColor("#000");
-            setBorder("1px solid #000");
-        } else if (currentColor === "3") {
-            setBackgroundColor("rgb(0,153,64)");
-            setBorder("1px solid #90ee90");
-        } else if (currentColor === "0") {
-            setBackgroundColor("rgb(0,153,64)")
-            setBorder("0px solid rgb(0,153,64)");
+        switch (currentColor) {
+            case '0':
+                setBackgroundColor("rgb(0,153,64)")
+                setBorder("0px solid rgb(0,153,64)");
+            break;
+            case '1':
+                setBackground("linear-gradient(30deg, rgba(255, 255, 255, 0.5) 0%," +
+                "rgba(203, 203, 203, 0.44) 11.02%, rgba(150, 150, 150, 0.38) 23.9%," +
+                "rgba(104, 104, 104, 0.32) 36.96%, rgba(67, 67, 67, 0.25) 49.92%," +
+                "rgba(37, 37, 37, 0.19) 62.76%, rgba(17, 17, 17, 0.12) 75.46%," +
+                "rgba(4, 4, 4, 0.06) 87.95%, rgba(0, 0, 0, 0) 100%)");
+                setBackgroundColor("#fff");
+                setBorder("1px solid #000");
+            break;
+            case '2':
+                setBackground("linear-gradient(210deg, rgba(255, 255, 255, 0.7) 0%," +
+                "rgba(203, 203, 203, 0.62) 11.02%, rgba(150, 150, 150, 0.53) 23.9%," +
+                "rgba(104, 104, 104, 0.44) 36.96%, rgba(67, 67, 67, 0.35) 49.92%," +
+                "rgba(37, 37, 37, 0.26) 62.76%, rgba(17, 17, 17, 0.17) 75.46%," +
+                "rgba(4, 4, 4, 0.08) 87.95%, rgba(0, 0, 0, 0) 100%)");
+                setBackgroundColor("#000");
+                setBorder("1px solid #000");
+            break;
+            case "3":
+                setBackgroundColor("rgb(0,153,64)");
+                setBorder("1px solid #90ee90");
+            break;
+            default:
+                setBackgroundColor("rgb(0,153,64)")
+                setBorder("0px solid rgb(0,153,64)");
         }
     });
 
