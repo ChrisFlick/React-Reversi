5593695d19fced636b7a3eabc09c00c6af1b7cb0
Ramon Sanchez <songwright66@gmail.com>
2019-12-10 21:36:31 -0800

    Cleaned up Navbar files

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,19 +4,15 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div className="navbar">
-      {/* <p className="lobby"> */}
       <p>
         <a href="/lobbies">LOBBIES</a>
       </p>
-      {/* <p className="profile"> */}
       <p>
         <a href="/profile">PROFILE</a>
       </p>
-      {/* <p className="logout"> */}
       <p>
         <a href="/">LOGOUT</a>
       </p>
-      {/* <p className="battle"> */}
       <p>
         <a href="/aigame">
         <p>BATTLE</p>
