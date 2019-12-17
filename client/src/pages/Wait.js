8570856b5bc52c24798e2283336d381dbe068c50
Ramon Sanchez <songwright66@gmail.com>
2019-12-17 13:04:49 -0800

    Added loading.gif to Wait page

--- client/src/pages/Wait.js
+++ client/src/pages/Wait.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Card from "../components/Card"
 import API from "../utils/API"
+import loading from "../img/loading.gif"
 import "../css/Lobbies.css"
 
 const lobbyId = localStorage.getItem('lobby')
@@ -25,6 +26,7 @@ const Lobbies = () => {
     return (
         <div className="wait-container">
             <Card>
+                <img src={loading} alt="loading"/>
                 <h1>Waiting for opponent</h1>
             </Card>
         </div>
