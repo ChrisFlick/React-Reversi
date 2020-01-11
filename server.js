bf5cd33c7a03e7d54a1cb50f6f8777759571e547
Chris Flick <chrisflick@gmail.com>
2020-01-10 18:56:06 -0800

    Cleaning up code

--- server.js
+++ server.js
@@ -16,18 +16,20 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'client/build')));
 // We need to use sessions to keep track of our user's login status
 // app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-
-
-// Requiring our routes
-require("./routes/api-routes.js")(app);
-
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
+
+// Requiring our routes
+require("./routes/api-routes.js")(app);
+
+
+
+
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
